4b0d52c08138753d40650570677a19ba4e9fc4fc
欧阳志鹏 <925975498@qq.com>
2021-08-19 17:05:41 +0800

    删除与修改

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,6 @@ from api import views
 
 urlpatterns = [
     path(r'books/', views.BookAPIView.as_view()),
+    path(r'books2/', views.BookView.as_view()),
     re_path(r'^books/(?P<pk>\d+)$', views.BookAPIView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -34,10 +34,7 @@ class BookAPIView(APIView):
     def put(self, request, *args, **kwargs):
         if kwargs.get('pk', None):
             book = models.Book.objects.filter(pk=kwargs.get('pk', None)).first()
-            book_ser = BookModelSerializer(instance=book, data=request.data)
-            book_ser.is_valid(raise_exception=True)
-            book_ser.save()
-            return APIResponse(data=book_ser.data)
+            book_ser = BookModelSerializer(instance=book, data=request.data, partial=True)
         else:
             book_list = []
             modify_data = []
@@ -46,16 +43,37 @@ class BookAPIView(APIView):
                 book = models.Book.objects.get(pk=pk)
                 book_list.append(book)
                 modify_data.append(item)
-            # 修改多个, 方案1, for循环
+            # 批量修改, 方案1, for循环
             # for i, si_data in enumerate(modify_data):
             #     book_ser = BookModelSerializer(instance=book_list[i], data=si_data)
             #     book_ser.is_valid(raise_exception=True)
             #     book_ser.save()
 
-            # 方案2, 重写ListSerializer的update方法
+            # 方案2, 重写ListSerializer的update方法, 在序列化类中编写
             book_ser = BookModelSerializer(instance=book_list, data=modify_data, many=True)
-            book_ser.is_valid(raise_exception=True)
-            book_ser.save()
 
-            return APIResponse(data=book_ser.data)
+        book_ser.is_valid(raise_exception=True)
+        book_ser.save()
 
+        return APIResponse(data=book_ser.data)
+
+    def delete(self, request, *args, **kwargs):
+        # 单删与批量删
+        pk = kwargs.get('pk')
+
+        pks = []
+        if pk:
+            pks.append(pk)
+        else:
+            pks = request.data.get('pks')
+        # ret返回受影响的行数
+        ret = models.Book.objects.filter(pk__in=pks, is_delete=False).update(is_delete=True)
+        if ret:
+            return APIResponse()
+        else:
+            return APIResponse(code=110, msg='没有要删除的数据')
+
+from rest_framework.generics import ListAPIView
+class BookView(ListAPIView):
+    queryset = models.Book.objects.all()
+    serializer_class = BookModelSerializer
